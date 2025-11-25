7b056399de8f6b0e7e14f567a6b0fa5491ef6562
Julio Quintero <jequinterope@unal.edu.co>
2025-11-24 23:09:47 -0500

    PATHCH: Avoid overrideng variables if they are alredy set

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -10,8 +10,14 @@ fatal()
 # TODO: handle override environment variables by cmd arguments
 # or enable the final user to specify a path to a .env file
 # that has to be mounted in container
-export ENVIRONMENT=prod
-export TIMEZONE=UTC
-export APP='{"host": "0.0.0.0", "port": 8080}'
+if [[ -z "$ENVIRONMENT" ]]; then
+    export ENVIRONMENT="prod"
+fi
+if [[ -z "$TIMEZONE" ]]; then
+    export TIMEZONE=UTC
+fi
+if [[ -z  "$APP" || -z "$APP__HOST" || -z "$APP__PORT" ]]; then
+    export APP='{"host": "0.0.0.0", "port": 8080}'
+fi
 
 python3 -m fastanalytics || fatal "Start python app"
